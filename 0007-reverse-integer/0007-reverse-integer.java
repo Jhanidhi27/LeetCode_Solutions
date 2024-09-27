b3073f101ea4e4e2f1ab7d527d9f8dd1d1402f39
Jhanidhi27 <142563589+Jhanidhi27@users.noreply.github.com>
2024-09-27 22:57:47 +0530

    Time: 1 ms (84.57%), Space: 40.5 MB (93.93%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -4,12 +4,12 @@ class Solution {
             return 0;
         }
         long reversed=0;
-        if(x>0)
+        if(x>0){
             while(x>0){
                 int digit=x%10;
                 reversed=(reversed*10)+digit;
                  x=x/10;
-             }
+             }}
         else{
             x*=(-1);
             while(x>0){
